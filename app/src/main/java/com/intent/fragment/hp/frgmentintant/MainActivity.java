15edede424e8856a6ee4be641fca89814a10f3bb
rahulbanti36 <rahulbanti36@gmail.com>
2019-01-13 23:46:23 +0530

    message display intent work

--- app/src/main/java/com/intent/fragment/hp/frgmentintant/MainActivity.java
+++ app/src/main/java/com/intent/fragment/hp/frgmentintant/MainActivity.java
@@ -1,14 +1,21 @@
 package com.intent.fragment.hp.frgmentintant;
 
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements menuBar.listItemClicked {
 EditText edtName;
 Button btnSubmit,btnInfo;
+Intent intent;
+int ActivityCodeForForm=1;
+FragmentManager manager=this.getSupportFragmentManager();;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +24,31 @@ Button btnSubmit,btnInfo;
         btnSubmit=findViewById(R.id.btnSubmit);
         edtName=findViewById(R.id.edtName);
         btnInfo.setVisibility(View.GONE);
+  manager.beginTransaction().hide(manager.findFragmentById(R.id.iconFragment)).commit();
+  btnSubmit.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+  intent=new Intent(MainActivity.this,com.intent.fragment.hp.frgmentintant.messageDisplay.class);
+  if(edtName.getText().toString().isEmpty()){
+      Toast.makeText(MainActivity.this,"plese fill name ",Toast.LENGTH_SHORT).show();
+  }
+  else
+  {
+      intent.putExtra("name",edtName.getText().toString().trim());
+      startActivity(intent);
+  }
+
+      }
+  });
+        btnInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent=new Intent(MainActivity.this,com.intent.fragment.hp.frgmentintant.infoForm.class);
+                startActivityForResult(intent,ActivityCodeForForm);
+
+            }
+        });
+
     }
 
     @Override
@@ -34,6 +66,17 @@ Button btnSubmit,btnInfo;
         else
         {
 
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==ActivityCodeForForm){
+            if(requestCode==RESULT_OK)
+            {
+                manager.beginTransaction().show(manager.findFragmentById(R.id.iconFragment)).commit();
+            }
         }
     }
 }
--- app/src/main/java/com/intent/fragment/hp/frgmentintant/messageDisplay.java
+++ app/src/main/java/com/intent/fragment/hp/frgmentintant/messageDisplay.java
@@ -2,12 +2,27 @@ package com.intent.fragment.hp.frgmentintant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class messageDisplay extends AppCompatActivity {
-
+TextView tvMsg;
+Button btnOk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_display);
+        btnOk=findViewById(R.id.btnBack);
+        tvMsg=findViewById(R.id.tvMsg);
+        String msg=getIntent().getStringExtra("name").toString();
+        msg=msg+" welcome to  message  activity";
+        tvMsg.setText(msg);
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                messageDisplay.this.finish();
+            }
+        });
     }
 }
